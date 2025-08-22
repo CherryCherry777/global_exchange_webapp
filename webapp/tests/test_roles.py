438123d32657bb20ad753901e6c519fe1abf8745
Cielito <cielitomelga7@gmail.com>
2025-08-22 17:12:11 -0300

    Presentacion 22/08

--- webapp/tests/test_roles.py
+++ webapp/tests/test_roles.py
@@ -7,18 +7,18 @@ User = get_user_model()
 class RoleTests(TestCase):
 
     def setUp(self):
-        self.admin_group, _ = Group.objects.get_or_create(name="Administrator")
-        self.employee_group, _ = Group.objects.get_or_create(name="Employee")
+        self.admin_group, _ = Group.objects.get_or_create(name="Administrador")
+        self.employee_group, _ = Group.objects.get_or_create(name="Empleado")
         self.user = User.objects.create_user(username='roleuser', password='pass123')
 
     def test_assign_default_role(self):
-        self.assertTrue(self.user.groups.filter(name="Regular User").exists())
+        self.assertTrue(self.user.groups.filter(name="Usuario").exists())
 
     def test_add_role(self):
         self.user.groups.add(self.employee_group)
-        self.assertTrue(self.user.groups.filter(name="Employee").exists())
+        self.assertTrue(self.user.groups.filter(name="Empleado").exists())
 
     def test_remove_role(self):
         self.user.groups.add(self.employee_group)
         self.user.groups.remove(self.employee_group)
-        self.assertFalse(self.user.groups.filter(name="Employee").exists())
+        self.assertFalse(self.user.groups.filter(name="Empleado").exists())
